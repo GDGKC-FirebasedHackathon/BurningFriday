4984091583296758c00468367d0db7a5ae513aa7
ParkJiyoung <realplay23@naver.com>
2017-02-18 02:20:23 +0900

    AlbaInfoFragment 설정

--- PartiSpot/app/src/main/java/com/gdgkorea/partispot/AlbaInfoFragment.java
+++ PartiSpot/app/src/main/java/com/gdgkorea/partispot/AlbaInfoFragment.java
@@ -6,39 +6,101 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class AlbaInfoFragment extends Fragment {
 
+    View view;
     ImageView imageView_photo;
     EditText editText_name, editText_birthday, editText_phone, editText_major, editText_intro;
-    MenuItem menuItem;
+    boolean changeState;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        setHasOptionsMenu(true);
+        view = inflater.inflate(R.layout.fragment_alba_info, container, false);
 
+        initModel();
         initView();
 
+        setEditor();
+        return view;
+    }
 
-        return inflater.inflate(R.layout.fragment_alba_info, container, false);
+    @Override
+    public void onResume() {
+        super.onResume();
+        // destroy all menu and re-call onCreateOptionsMenu
+        getActivity().invalidateOptionsMenu();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_alba_info, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_setAlbaInfo :
+                if(!changeState) {
+                    changeState = true;
+                    item.setIcon(R.mipmap.ic_save);
+                }
+                else {
+                    changeState = false;
+                    item.setIcon(R.mipmap.ic_edit);
+                }
+                setEditor();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void initView() {
-        imageView_photo = (ImageView) getView().findViewById(R.id.imageView_photo);
+        view = View.inflate(getContext(), R.layout.fragment_alba_info, null);
 
-        editText_name = (EditText) getView().findViewById(R.id.editText_name);
-        editText_birthday = (EditText) getView().findViewById(R.id.editText_birthday);
-        editText_phone = (EditText) getView().findViewById(R.id.editText_phone);
-        editText_major = (EditText) getView().findViewById(R.id.editText_major);
-        editText_intro = (EditText) getView().findViewById(R.id.editText_intro);
+        imageView_photo = (ImageView) view.findViewById(R.id.imageView_photo);
 
-        menuItem = (MenuItem) getView().findViewById(R.id.action_settings);
+        editText_name = (EditText) view.findViewById(R.id.editText_name);
+        editText_birthday = (EditText) view.findViewById(R.id.editText_birthday);
+        editText_phone = (EditText) view.findViewById(R.id.editText_phone);
+        editText_major = (EditText) view.findViewById(R.id.editText_major);
+        editText_intro = (EditText) view.findViewById(R.id.editText_intro);
 
-        menuItem.setTitle("내 정보 수정");
+    }
+
+    private void initModel() {
+        changeState = false;
+    }
+
+    private void setEditor() {
+
+        editText_name.setFocusable(changeState);
+        editText_birthday.setFocusable(changeState);
+        editText_phone.setFocusable(changeState);
+        editText_major.setFocusable(changeState);
+        editText_intro.setFocusable(changeState);
+
+        editText_name.setFocusableInTouchMode(changeState);
+        editText_birthday.setFocusableInTouchMode(changeState);
+        editText_phone.setFocusableInTouchMode(changeState);
+        editText_major.setFocusableInTouchMode(changeState);
+        editText_intro.setFocusableInTouchMode(changeState);
+
+        editText_name.setClickable(changeState);
+        editText_birthday.setClickable(changeState);
+        editText_phone.setClickable(changeState);
+        editText_major.setClickable(changeState);
+        editText_intro.setClickable(changeState);
     }
 }
